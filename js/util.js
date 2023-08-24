e3d32b0a1ebd15763661a9101183292778430880
Наталья Зиновьева <natalizin85@gmail.com>
2023-08-24 17:40:55 +1000

    util

--- js/util.js
+++ js/util.js
@@ -18,7 +18,8 @@ const showAlertMessage = (message) => {
   alertMessage.style.zIndex = '100';
   alertMessage.style.position = 'absolute';
   alertMessage.style.height = 'max-content';
-  alertMessage.style.left = `calc(50% - ${alertMessage.length}px)`;
+  alertMessage.style.left = '0';
+  alertMessage.style.right = '0';
   alertMessage.style.top = '40%';
   alertMessage.style.padding = '10px 3px';
   alertMessage.style.fontSize = '30px';
